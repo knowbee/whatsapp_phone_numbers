f31ad800cb0250a0fcd434d0840dd6ad9e6f98eb
Googler <knowbeeinc@gmail.com>
2020-04-07 16:44:13 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,40 @@
+# Whatsapp Phone Numbers Scraper
+
+- Target : `https://web.whatsapp.com/`
+
+## Requirements
+
+- Chrome Webdriver
+- Python >= 3.5
+
+## Setup
+
+```
+git clone https://knowbee@bitbucket.org/fastaio/whatsapp_phone_numbers.git
+```
+
+```
+cd whatsapp_phone_numbers
+```
+
+```
+python -m venv .
+```
+
+```
+activate.bat
+```
+
+```
+pip install -r requirements.txt
+```
+
+## Usage
+
+```
+  python run.py
+```
+
+## Author
+
+Igwaneza Bruce <knowbeeinc@gmail.com>
---
+++ run.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python
+from selenium import webdriver
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+import time
+import json
+import re
+import os
+numbers = []
+driver = webdriver.Chrome()
+wait = WebDriverWait(driver, 600)
+
+
+def scrape():
+    i = 0
+    groups_people = driver.find_elements_by_xpath(
+        '//*[@id="pane-side"]/div[1]/div/div/div')
+    for p in groups_people:
+        p.click()
+        x_arg = '//*[@id="main"]/header/div[2]'
+        user = wait.until(EC.presence_of_element_located((By.XPATH, x_arg)))
+        header = p.find_element_by_xpath('//*[@id="main"]/header/div[2]')
+        title = header.find_element_by_xpath(
+            '//*[@id="main"]/header/div[2]/div[1]/div/span').text
+        header.click()
+        time.sleep(2)
+        phone = p.find_element_by_xpath(
+            '/html/body/div[1]/div/div/div[2]/div[3]/span/div/span/div/div/div[1]').text.split("\n")[-4]
+        print(".")
+        if ', ' in phone:
+            numbers + phone.split(", ")
+        numbers.append(phone.replace(" ", ""))
+        i += 1
+    with open("dirty_contacts.json", "w") as file:
+        json.dump(numbers, file)
+
+
+if __name__ == '__main__':
+    driver.get("https://web.whatsapp.com/")
+    wait = WebDriverWait(driver, 600)
+    x_arg = '//*[@id="pane-side"]/div[1]/div/div/div[1]'
+    chats = wait.until(EC.presence_of_element_located((By.XPATH, x_arg)))
+    scrape()
+    with open("dirty_contacts.json", encoding="utf-8") as file:
+        contacts = json.load(file)
+        array_of_contacts = []
+        unique_contacts = []
+        for contact in contacts:
+            if len(contact) > 1:
+                array_of_contacts = array_of_contacts + contact.split(",")
+
+        for contact in array_of_contacts:
+            if contact not in unique_contacts and re.match("\+|[0-9]", str(contact)):
+                unique_contacts.append(contact)
+
+        with open("clean_contacts.json", "w") as file:
+            json.dump(unique_contacts, file)
+
+os.remove("dirty_contacts.json")
+print("done")
+driver.quit()
